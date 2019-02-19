432560ee91b5c5388e4b27da7825ac1f7b6c6648
jcosentino <johnsc301@gmail.com>
2019-02-18 22:37:34 -0500

    [2/18/19] - 10:36pm - added basic database Flask pieces and simple POST route

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,40 @@
+#Need to modularize the Flask logic
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:8milerun@localhost/testdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Turn off annoying message
+db = SQLAlchemy(app)
+
+#Should create schema if it doesn't already exist
+#
+
+class Person(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String(80), unique=False, nullable=False)
+
+db.create_all()
+testQueryObj = Person.query.get(1)
+print('Here is the test query:\t {0}'.format(testQueryObj.name))
 
 @app.route('/')
 def hello():
+	print('Hello world!')
 	return 'Hello world!'
+
+@app.route('/nothing')
+def handleNothing():
+	return 'Nothing!'
+
+#BROKEN
+@app.route('/signup/?<string:name>', methods = ['GET', 'POST'])
+def signup(name):
+	if request.method == 'GET':
+		return Person.query.filter_by(name=name)
+	if request.method == 'POST':
+		db.session.add(name)
+		db.session.commit()
 
 if __name__ == '__main__':
 	app.run()
--- src/Signup/signup.jsx
+++ src/Signup/signup.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import './signup.css';
+import axios from 'axios';
 
 class Signup extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			name: ''
+		}
+	}
+
+	handleChange = (event) => {
+		this.setState({ name: event.target.value });
+	}
+
+	handleSubmit = async(event) => {
+		try{
+			event.preventDefault(); //What is this?
+			await axios.post(`http://127.0.0.1:5000/signup`, {'name': this.state.name});
+		} catch(err){
+			console.log(err);
+		}
+	}
+
 	render(){
 		return (
 			<div className='signup'>
-				<form action='/signup' method='post'>
-					<input type='text' name='email'/><br/>
-					<input type='password' name='password'/><br/>
+				<form onSubmit={this.handleSubmit}>
+					<input type='text' name='name' onChange={this.handleChange}/><br/>
 					<input type='submit' value='Submit'/>
 				</form>
 			</div>
